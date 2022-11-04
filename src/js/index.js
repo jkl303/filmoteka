5380ce6388a343a00d97ff247f92f0ec17f16800
ChemerysIgor <chemeris_igor@ukr.net>
2022-11-05 01:58:16 +0200

    creare home-page
    получено информацию о популярных фильмах и осуществлено рендер разметки, добавлены стили для домашней страници! пока не удалось зарендерить названия жанров и сделать разметку через хенделбарс

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,40 @@
 import { getCurrentPage } from './getCurrentPage';
+import { Fetchmovie } from './homepagerender';
+// import markupMovie from '../templates/movie-card.hbs';
+const list = document.querySelector('.movie-list');
 
-// Adds a red line (active page) under "Home or My Library links in the header"
 getCurrentPage();
+
+Fetchmovie()
+  .then(data => {
+    return data.results;
+  })
+  .then(movies => {
+    list.innerHTML = '';
+    list.insertAdjacentHTML(`beforeend`, markupMovie(movies));
+    function markupMovie(array) {
+      return array
+        .map(
+          arr =>
+            `<li class='movie-item'>
+      <a href= '${arr.id}' class='movie-link'>
+        <img src='https://image.tmdb.org/t/p/w500/${arr.poster_path}' alt='${
+              arr.title
+            }' class='movie-image' />
+        <div class='movie-info'>
+          <p class='movie-title'>${arr.title}</p>
+          <div class="movie-conteiner">
+          <p class='movie-description'>${arr.genre_ids} |
+          ${arr.release_date.slice(0, 4)}</p>
+          <p class ="movie-ratting is-hidden"> ${arr.vote_average}</p>
+          </div>
+        </div>
+      </a>
+    </li>`
+        )
+        .join('');
+    }
+  })
+  .catch(error => {
+    console.log(error.message);
+  });
