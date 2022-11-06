406a875e58eadfcec2c6a36c6a22f51b16887888
Nazar D <nazardonets@gmail.com>
2022-11-06 19:48:41 +0200

    bugfix/fixed_issue_with_openModal_function

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import { getCurrentPage } from './getCurrentPage';
-// import { modalMovie } from './modal-movie';
+import { openModal } from './modal-movie';
 import { renderUI } from './renderHomePageUI';
 
 // Adds a red line under active page in the website header
@@ -7,6 +7,3 @@ getCurrentPage();
 
 // UI render invocation
 renderUI();
-
-// Add modal-movie
-// modalMovie();
