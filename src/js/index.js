e27ed08f2efcd2db705d0deea3a90ef55ceba165
AlexTanetsDev <alexTanetsDev@gmail.com>
2022-11-06 21:16:10 +0200

    add loader and input logic and notify

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 import { getCurrentPage } from './getCurrentPage';
 // import { modalMovie } from './modal-movie';
 import { renderUI } from './renderHomePageUI';
-
+import { onSubmit } from './searchinputLogic';
 // Adds a red line under active page in the website header
 getCurrentPage();
 
 // UI render invocation
 renderUI();
+
+
+// searchInputLogic
+
+const searchForm = document.querySelector('.search-form');
+searchForm.addEventListener('submit', onSubmit);
