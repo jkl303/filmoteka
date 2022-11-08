d9c9a992e08ad5e1a8289b54d032b77e3de59248
KovalchukAnastasia <103355891+KovalchukAnastasia@users.noreply.github.com>
2022-11-09 01:20:17 +0200

    tablet & desktop styles

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -6,7 +6,7 @@ export async function openModal(movie_id) {
   const movie_url_original = `${BASE_URL}/movie/${movie_id}?api_key=${API_KEY}`;
   const modalEl = document.querySelector('.modal');
 
-  // console.log(movie_url_original)
+  //   console.log(movie_url_original);
   const resp = await fetch(movie_url_original, {
     headers: {
       'Content-Type': 'application/json',
@@ -17,31 +17,58 @@ export async function openModal(movie_id) {
   modalEl.classList.add('modal--show');
   document.body.classList.add('stop-scroll');
   modalEl.innerHTML = `<div class="modal__card">
-        <img src="${IMG_URL}${respData.poster_path}" alt="${IMG_URL}${
-    respData.backdrop_path}" class="modal__movie-backdrop" width="375"/>
+        <img src="${IMG_URL}${
+    respData.poster_path
+  }" alt="" class="modal__movie-backdrop"/>
     
         <div class="modal__wrap">
         <h2>
-            <span class="modal__movie-title">${respData.original_title}</span>
+            <span class="modal__movie-title">${respData.title}</span>
         </h2>
         <ul class="modal__movie-info">
-          <li class="modal__movie-vote"> Vote / Votes <span class="movie-vote-upper">${respData.vote_average.toFixed(
+          <li>
+            <div class="modal__movie-vote-cont">          
+                <p class="modal__movie-vote">Vote / Votes </p>
+                    <div class="movie-vote-upper">${
+                      respData.vote_average.toFixed(1) &&
+                      `<div class="movie-vote-upper movie__average--${getClassByRate(
+                        respData.vote_average.toFixed(1)
+                      )}">
+                      ${respData.vote_average.toFixed(1)}
+                        </div>`
+                    } / ${respData.vote_count}
+                    </div>
+                
+            </div>
+          </li>
+          <li>
+          <div class="modal__movie-popularity-cont">
+          <p class="modal__movie-popularity">Popularity
+          <span class="movie-popularity-upper">${respData.popularity.toFixed(
             1
-          )}/${respData.vote_count}</span></li>
-          <li class="modal__movie-popularity">Popularity <span class="movie-popularity-upper">${respData.popularity.toFixed(
-            1
-          )}</span></li>
-          <li class="modal__movie-original-title">Original Title <span class="movie-title-upper">${
+          )}</span>
+          </p>
+          </div>
+
+         </li>
+          <li> 
+          <p class="modal__movie-original-title">Original Title <span class="movie-title-upper">${
             respData.original_title
-          }</span></li>
-          <li class="modal__movie-genre">Genre <span class="movie-genre-upper">${respData.genres
+          }</span></p>  
+          </li>
+          <li>
+          <p class="modal__movie-genre">Genre <span class="movie-genre-upper">${respData.genres
             .map(elem => `${elem.name}`)
-            .join(', ')}</span></li>
-          <li class="modal__movie-overview">ABOUT <span class="movie-overview-upper">${
-            respData.overview
-          }</span></li>
+            .join(', ')}</span></p>
+         
+            </li>
+          <li>
+          <p class="modal__movie-overview">ABOUT</p>
+          <span class="movie-overview-upper">${respData.overview}</span>
+          </li>
         </ul>
-        <button type="button" class="modal__btn-close">X</button>
+        
+        <button type="button" class="modal__btn-close"></button>
         <div class="modal__btn-list">
         <button type="button" class="modal__btn-watched">ADD TO WATCHED</button>
         <button type="button" class="modal__btn-queue">ADD TO QUEUE</button>
@@ -61,6 +88,7 @@ function closeModal() {
 }
 
 window.addEventListener('click', evt => {
+  // console.log(evt.target)
   const modalEl = document.querySelector('.modal');
   if (evt.target === modalEl) {
     closeModal();
@@ -72,3 +100,15 @@ window.addEventListener('keydown', evt => {
     closeModal();
   }
 });
+
+// цветовое решение для рейтинга
+
+function getClassByRate(vote) {
+  if (vote >= 7) {
+    return 'green';
+  } else if (vote > 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
