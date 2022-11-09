a6f300ec02080aed8a66fa578b04ab9e13e72255
jkl303 <303vladzoz@gmail.com>
2022-11-09 18:01:40 +0200

    added sorting to homepage

--- src/js/homePageSorting.js
+++ src/js/homePageSorting.js
@@ -2,11 +2,15 @@ import { API_KEY, BASE_URL, IMG_URL } from './api-service';
 import axios from 'axios';
 import { addLoader, removeLoader } from './loader';
 const container = document.querySelector('main .container');
+const range = document.querySelector("input[type='range']");
+const bubble = document.querySelector('.bubble');
+const title = document.querySelector('.page-heading');
 
 export async function byName(value) {
   try {
     if (value === 'empty') {
     } else {
+      title.textContent = `Sorted by title(${value}ending)`;
       const {
         data: { results },
       } = await axios.get(`${BASE_URL}/discover/movie?sort_by=title.${value}`, {
@@ -24,6 +28,7 @@ export async function byName(value) {
 
 export async function byYear(year) {
   try {
+    title.textContent = `Movies released in ${year}`;
     const {
       data: { results },
     } = await axios.get(
@@ -39,4 +44,14 @@ export async function byYear(year) {
   } catch (err) {
     return err;
   }
+}
+
+export function setBubble() {
+  bubble.style.visibility = 'visible';
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  bubble.innerHTML = val;
+  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
 }
--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,21 @@ import { getCurrentPage } from './getCurrentPage';
 // import { modalMovie } from './modal-movie';
 import { renderUI } from './renderHomePageUI';
 import { onSubmit } from './searchinputLogic';
+import { byName, byYear, setBubble } from './homePageSorting';
 // Adds a red line under active page in the website header
 getCurrentPage();
 
 // UI render invocation
 renderUI();
 
-
-// searchInputLogic
+searchInputLogic();
 
 const searchForm = document.querySelector('.search-form');
 searchForm.addEventListener('submit', onSubmit);
+
+const byNameSelect = document.querySelector('[name="by-name__select"]');
+byNameSelect.addEventListener('change', () => byName(byNameSelect.value));
+
+const byYearInput = document.querySelector('[name="by-year"]');
+byYearInput.addEventListener('input', setBubble);
+byYearInput.addEventListener('change', () => byYear(byYearInput.value));
