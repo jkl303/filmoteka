b5f933acb8fd87fe5117e6586df39fbc38320e7e
ChemerysIgor <chemeris_igor@ukr.net>
2022-11-09 23:46:25 +0200

    change themes
    added icons to change themes from light to dark and reverse

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,23 @@ import { getCurrentPage } from './getCurrentPage';
 // import { modalMovie } from './modal-movie';
 import { renderUI } from './renderHomePageUI';
 import { onSubmit } from './searchinputLogic';
+import { becomeDark } from './changeTheme';
+import { becomeLight } from './changeTheme';
+import { storageChecker } from './changeTheme';
 // Adds a red line under active page in the website header
 getCurrentPage();
 
 // UI render invocation
 renderUI();
 
-
 // searchInputLogic
 
 const searchForm = document.querySelector('.search-form');
 searchForm.addEventListener('submit', onSubmit);
+
+// change themes
+storageChecker();
+const dark = document.querySelector('[data-theme ="dark"]');
+dark.addEventListener('click', becomeDark);
+const light = document.querySelector('[data-theme ="light"]');
+light.addEventListener('click', becomeLight);
