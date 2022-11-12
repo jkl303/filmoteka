5eb8820e6d2746444e7ae64faf25b2a1116277fb
AlexTanetsDev <alexTanetsDev@gmail.com>
2022-11-12 22:50:55 +0200

    fins

--- src/js/modal-footer.js
+++ src/js/modal-footer.js
@@ -9,6 +9,20 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
+
+  window.addEventListener('click', evt => {
+    
+    if (evt.target === refs.modal) {
+      toggleModal();
+    }
+  });
+
+  window.addEventListener('keydown', evt => {
+    if (evt.code === "Escape" && !refs.modal.classList.contains("is-hidden")) {
+      toggleModal();
+    }
+  });
+
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
     refs.body.classList.toggle('no-scroll')
